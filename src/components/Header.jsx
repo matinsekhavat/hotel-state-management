85fd0c6d0904fe27106b9d4bbf621c6cf16bc2ba
matinsekhavat <matinhyphen@gmail.com>
2024-06-01 15:06:42 +0330

    add static ui in header files v1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,42 @@
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+import { useState } from "react";
 function Header() {
-  return <div className="header">Header</div>;
+  const [destination, setDestination] = useState("");
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            type="text"
+            placeholder="where to go?"
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2024/04/04</div>
+          <span className="seperator"></span>
+        </div>
+        <span className="seperator"></span>
+        <div className="headerSearchItem">
+          <div id="optionDropDown">1 Adult &bull; 2 children &bull; 1 room</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
