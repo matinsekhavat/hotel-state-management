61087eec3d3bf8c8c7fbaaeb303fbcafeb3be59e
matinsekhavat <matinhyphen@gmail.com>
2024-06-02 16:20:28 +0330

    make exception area for useOutsideClick hook

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 
-function useOutsideClick(ref, cb) {
+function useOutsideClick(ref, cb, exceptionId) {
   useEffect(() => {
     const clickHandler = (e) => {
-      if (ref.current && !ref.current.contains(e.target)) {
+      if (
+        ref.current &&
+        !ref.current.contains(e.target) &&
+        ref.current.id === exceptionId
+      ) {
         cb();
       }
     };
