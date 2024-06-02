4b1305b23e6d408ecbd2aec688d1983bb90dfde1
matinsekhavat <matinhyphen@gmail.com>
2024-06-02 14:48:53 +0330

    implement needed code for useOutsideClick for openOptions config in GuestOption List

--- src/components/GuestOptionList.jsx
+++ src/components/GuestOptionList.jsx
@@ -1,18 +1,24 @@
+import { useRef } from "react";
 import GuestOptionItem from "./GuestOptionItem";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function GuestOptionList({
   options,
   setOptions,
   increaseOptionsHandler,
   decreaseOptionsHandler,
+  setOpenOptions,
 }) {
   const geustOptionList = [
     { id: 1, value: "adult", minLimit: 1 },
     { id: 2, value: "children", minLimit: 0 },
     { id: 3, value: "room", minLimit: 1 },
   ];
+  const optionsRef = useRef();
+  useOutsideClick(optionsRef, () => setOpenOptions((prev) => !prev));
+
   return (
-    <div className="guestOptions">
+    <div className="guestOptions" ref={optionsRef}>
       {geustOptionList.map((guest) => (
         <GuestOptionItem
           key={guest.id}
