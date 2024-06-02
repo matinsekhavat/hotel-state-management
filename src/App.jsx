3c3b12d8362751a03fbe8454a56d33338a448363
matinsekhavat <matinhyphen@gmail.com>
2024-06-02 22:06:56 +0330

    add toaser component & LocationList component to Root file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header";
+import LocationList from "./components/LocationList";
 
 function App() {
   return (
     <>
+      <Toaster />
       <Header />
+      <LocationList />
     </>
   );
 }
