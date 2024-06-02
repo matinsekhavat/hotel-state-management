9a9dc7cdcfa0dff2165049a84a52b8cb8e53ac2e
matinsekhavat <matinhyphen@gmail.com>
2024-06-02 23:28:41 +0330

    make nested Route test version  for /hotels

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import LocationList from "./components/LocationList";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HotelLayout from "./components/HotelLayout";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<LocationList />} />
+          <Route path="/hotels" element={<HotelLayout />}>
+            <Route index element={<div>all</div>} />
+            <Route path=":id" element={<div>singles</div>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
