cfd2389b8221181a89440b5567e95a754f485f7a
matinsekhavat <matinhyphen@gmail.com>
2024-06-04 15:45:41 +0330

    sync mapcenter to previous lat,lng when user history goes back

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,22 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotel } from "../context/HotelsProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const { hotelLoading, hotels } = useHotel();
   const [position, setPosition] = useState([48.4, 2.9]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  useEffect(() => {
+    if (lat && lng) {
+      console.log(lat, lng);
+      setPosition([lat, lng]);
+    }
+  }, [lat, lng]);
+  console.log(position);
+
   return (
     <div className="mapContainer">
       <MapContainer
@@ -12,19 +24,17 @@ function Map() {
         className="map"
         zoom={5}
         scrollWheelZoom={true}
+        key={crypto.randomUUID()}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
+        <ChangeCenter position={position} />
         {hotels.map((item) => (
-          <>
-            <Marker position={[item.latitude, item.longitude]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </>
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
+            <Popup>{item.host_location}</Popup>
+          </Marker>
         ))}
       </MapContainer>
     </div>
@@ -32,3 +42,9 @@ function Map() {
 }
 
 export default Map;
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
