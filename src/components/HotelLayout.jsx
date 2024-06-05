c3a774651497406ebe197d6a4022123fe5e8e5ae
matinsekhavat <matinhyphen@gmail.com>
2024-06-06 00:23:32 +0330

    make Map component reusable

--- src/components/HotelLayout.jsx
+++ src/components/HotelLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Map from "./Map";
+import { useHotel } from "../context/HotelsProvider";
 
 function HotelLayout() {
+  const { hotels } = useHotel();
   return (
     <div className="appLayout">
       <div className="sidebar">
         <Outlet />
       </div>
-      <Map />
+      <Map locations={hotels} />
     </div>
   );
 }
