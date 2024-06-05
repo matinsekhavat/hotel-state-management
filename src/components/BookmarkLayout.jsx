21e44bf275e17ae490f649bb5a1ea2c9231c33b7
matinsekhavat <matinhyphen@gmail.com>
2024-06-06 00:15:34 +0330

    add base JSX we need into the BookmarkLayout.jsx

--- src/components/BookmarkLayout.jsx
+++ src/components/BookmarkLayout.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Map from "./Map";
+
 function BookmarkLayout() {
-  return <div>BookmarkLayout</div>;
+  return (
+    <div className="appLayout">
+      <div className="sidebar">
+        <Outlet />
+      </div>
+      <Map />
+    </div>
+  );
 }
 
 export default BookmarkLayout;
