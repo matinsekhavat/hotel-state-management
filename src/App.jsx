97ab6300cef962a0a71cf82d406fae130214a4d3
matinsekhavat <matinhyphen@gmail.com>
2024-06-08 01:07:57 +0330

    wrapp Bookmark provider ro entire app

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,28 @@ import Hotels from "./components/Hotels";
 import HotelsProvider from "./context/HotelsProvider";
 import SingleHotel from "./components/SingleHotel";
 import BookmarkLayout from "./components/BookmarkLayout";
+import BookmarkProvider from "./context/BookmarkProvider";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <HotelsProvider>
-          <Toaster />
-          <Header />
-          <Routes>
-            <Route path="/" element={<LocationList />} />
-            <Route path="/hotels" element={<HotelLayout />}>
-              <Route index element={<Hotels />} />
-              <Route path=":id" element={<SingleHotel />} />
-            </Route>
-            <Route path="/bookmark" element={<BookmarkLayout />}>
-              <Route index element={<div>bookmark list</div>} />
-              <Route path="add" element={<div>add</div>} />
-            </Route>
-          </Routes>
+          <BookmarkProvider>
+            <Toaster />
+            <Header />
+            <Routes>
+              <Route path="/" element={<LocationList />} />
+              <Route path="/hotels" element={<HotelLayout />}>
+                <Route index element={<Hotels />} />
+                <Route path=":id" element={<SingleHotel />} />
+              </Route>
+              <Route path="/bookmark" element={<BookmarkLayout />}>
+                <Route index element={<div>bookmark list</div>} />
+                <Route path="add" element={<div>add</div>} />
+              </Route>
+            </Routes>
+          </BookmarkProvider>
         </HotelsProvider>
       </BrowserRouter>
     </>
