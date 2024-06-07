1cfbb5036fcfe24ee5410997ce0ceed71b090b35
matinsekhavat <matinhyphen@gmail.com>
2024-06-08 01:11:38 +0330

    import list of bookmrks for BookmarkLayout component to pass as a prop into Map

--- src/components/BookmarkLayout.jsx
+++ src/components/BookmarkLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Map from "./Map";
+import { useBookmark } from "../context/BookmarkProvider";
 
 function BookmarkLayout() {
+  const { bookmarks } = useBookmark();
   return (
     <div className="appLayout">
       <div className="sidebar">
         <Outlet />
       </div>
-      <Map locations={[]} />
+      <Map locations={bookmarks} />
     </div>
   );
 }
