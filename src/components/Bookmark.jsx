0bdb3bc0f8c02bde9501bc9a6849aec603080eef
matinsekhavat <matinhyphen@gmail.com>
2024-06-08 15:22:27 +0330

    implement current bookmark when user click in each link then comeback to /bookmark can see prev bookmark

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,20 +1,30 @@
+import { Link } from "react-router-dom";
 import { useBookmark } from "../context/BookmarkProvider";
 import Loader from "./Loader";
 import ReactCountryFlag from "react-country-flag";
 
 function Bookmark() {
-  const { bookmarksLoading, bookmarks } = useBookmark();
+  const { bookmarksLoading, bookmarks, currentBookmark } = useBookmark();
   if (bookmarksLoading) return <Loader />;
   return (
     <div>
       <h2>Bookmarks 📌</h2>
       <div className="bookmarkList">
         {bookmarks.map((item) => (
-          <div className="bookmarkItem" key={item?.id}>
-            <ReactCountryFlag svg countryCode={item.countryCode} />
-            &nbsp; <strong>{item?.cityName}</strong> &nbsp;
-            <span>{item?.country}</span>
-          </div>
+          <Link
+            key={item.id}
+            to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
+          >
+            <div
+              className={`bookmarkItem ${
+                currentBookmark == item.id ? "current-bookmark" : ""
+              }`}
+            >
+              <ReactCountryFlag svg countryCode={item.countryCode} />
+              &nbsp; <strong>{item.cityName}</strong> &nbsp;
+              <span>{item.country}</span>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
